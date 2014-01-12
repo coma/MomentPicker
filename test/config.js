add557ae5f2f52f3d26590c0614b80b223ceafde
Eduardo García Sanz <eduardo@comakai.com>
2014-01-12 12:00:43 +0100

    Set initial date

--- test/config.js
+++ test/config.js
@@ -1,11 +1,9 @@
 test('config', function() {
 
-    var date = moment()
-        .add('d', Math.round(365 * Math.random()))
-        .startOf('day');
-
     var config = {
-        date: date
+        date: moment()
+            .add('d', Math.round(365 * Math.random()))
+            .startOf('day')
     };
 
     var picker = $('#picker').MomentPicker(config);
@@ -13,5 +11,5 @@ test('config', function() {
     var header = picker.children('div.header');
     var body = picker.children('div.body');
 
-    strictEqual(api.val().format(), date.format(), 'Initial date is correct.');
+    strictEqual(api.val().format(), config.date.format(), 'Initial date is correct.');
 });
